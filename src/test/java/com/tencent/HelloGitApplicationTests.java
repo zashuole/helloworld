468695755efb4bdc582b449f0db9846558eaf3eb
LDDDS <2030305719@qq.com>
2024-11-26 23:26:49 +0800

    Initial commit

---
+++ src/test/java/com/tencent/HelloGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tencent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
