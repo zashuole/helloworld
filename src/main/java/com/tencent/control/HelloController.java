ff93b40779f0429f7d7ec0b797765fbca87b30e3
LDDDS <2030305719@qq.com>
2024-11-28 12:04:35 +0800

    test..

--- src/main/java/com/tencent/control/HelloController.java
+++ src/main/java/com/tencent/control/HelloController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("hello")
 public class HelloController {
-
     @GetMapping
     public String hello() {
         return "Hello World";
